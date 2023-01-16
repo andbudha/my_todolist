9e4a063ce2ed74e4d69a882852ec7c850e68650b
andbudha <andreibartov@gmail.com>
2023-01-16 23:20:27 +0100

    app updated

---
+++ src/Todolist.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+type TodolistPropsType = {
+    title: string
+}
+export const Todolist = (props: TodolistPropsType) => {
+    return (
+        <div>
+            <div>
+                <h3>{props.title}</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
