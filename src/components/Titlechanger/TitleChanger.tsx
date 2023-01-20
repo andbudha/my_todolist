0609c75cea98647ce68aafd5a16aa978442706b7
andbudha <andreibartov@gmail.com>
2023-01-20 16:40:41 +0100

    comp. TitleChanger updated

--- src/components/Titlechanger/TitleChanger.tsx
+++ src/components/Titlechanger/TitleChanger.tsx
@@ -1,19 +1,28 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 type TitleChangerPropsType = {
     title: string
 }
 export const TitleChanger = (props: TitleChangerPropsType) => {
+
+    //input edit state
     const[editTitle, setEditTitle] = useState(false);
+
+    //input value state
+    const[newTitle, setNewTitle]=useState('');
 
     const setEditTitleHandler = () => {
       setEditTitle(!editTitle);
     }
 
+    const inputValueCatchingHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setNewTitle(event.currentTarget.value)
+    }
+
     return (
         <>
             {editTitle
-                ? <input value={props.title} autoFocus onBlur={setEditTitleHandler}/>
+                ? <input value={newTitle} autoFocus onBlur={setEditTitleHandler} onChange={inputValueCatchingHandler}/>
                 : <span onDoubleClick={setEditTitleHandler}>{props.title}</span>}
 
 
